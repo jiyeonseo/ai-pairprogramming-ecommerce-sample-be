4fd2084be2d7a462539372996d3cae66e4ec310d
Cheese <cheese@Cheeseui-MacBookPro.local>
2024-03-24 06:45:01 +0900

    add chat endpoint

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,9 @@
 import os
 class settings:
+    
+    open_ai_api_key: str = os.getenv("OPEN_AI_API_KEY")
+    assistant_id: str = os.getenv("ASSISTANT_ID")
+
     def get_database_url():
         if os.getenv("ENVIRONMENT","local") == "test":
             return "sqlite:///./test.db"
